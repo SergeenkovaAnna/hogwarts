a740881950e8109bef5bd5afc12177977e86874d
AnnaSergeenkova <lutekAnn@yandex.ru>
2022-03-24 21:40:54 +0300

    hw3.6

--- src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
@@ -1,12 +1,12 @@
 package ru.hogwarts.school.controller;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.RequestEntity;
 import ru.hogwarts.school.model.Student;
 
 
@@ -16,47 +16,63 @@ public class StudentControllerTest {
     @LocalServerPort
     private int port;
 
+    private static final int ID = 1;
+
+    private static final Student STUDENT = new Student();
+
+
     @Autowired
     private StudentController studentController;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @BeforeAll
+    public static void init() {
+        STUDENT.setName("Ann");
+        STUDENT.setAge(12);
+    }
+
     @Test
     public void testPostStudent() throws Exception {
-        Student student = new Student();
-        student.setName("Ann");
-        student.setAge(12);
+        init();
         Assertions
-                .assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/student", student, Student.class))
+                .assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/student", STUDENT, Student.class))
                 .isNotNull();
+    }
+
+//    @Test
+//    public void testGetStudents() throws Exception {
+//        Assertions
+//                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/student/{id}" + , Student.class))
+//                .isNotNull();
+//    }
+
+    @Test
+    public void testDeleteStudent() throws Exception {
+            Student response =this.restTemplate.postForObject("http://localhost:" + port + "/student", STUDENT, Student.class);
+            this.restTemplate.delete("http://localhost:" + port + "/student" + "/{id}", response.getId());
+            Student isDelete = this.restTemplate.getForObject("http://localhost:" + port + "/student" + "/{id}", Student.class, response.getId());
+        Assertions
+                .assertThat(isDelete.getId()).isNull();
+
+
+    }
+
+
+    private Student getStudentById() {
+        return this.restTemplate.getForObject("http://localhost:" + port + "/student/{id}", Student.class, ID);
+
     }
 
     @Test
-    public void testGetStudents() throws Exception {
+    public void testEditStudent() throws Exception {
+        Student studentBeforeEdit = getStudentById();
+        studentBeforeEdit.setName("Bill");
+        this.restTemplate.put("http://localhost:" + port + "/student", studentBeforeEdit);
+        Student afterEdit = getStudentById();
         Assertions
-                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/student", Student.class))
-                .isNotNull();
+                .assertThat(afterEdit).isEqualTo(studentBeforeEdit);
     }
-
-//    @Test
-//    public void testDeleteStudent() throws Exception {
-//        Student student = new Student();
-//        student.setName("Ann");
-//        student.setAge(12);
-//        Assertions
-//                .assertThat(this.restTemplate.delete("http://localhost:" + port + "/student" + "/{id}", id, Student.class))
-//                .isNotNull();
-//
-//    }
-//
-//    @Test
-//    public void testEditStudent() throws Exception {
-//        Student student = new Student();
-//        student.setName("Ann");
-//        student.setAge(12);
-//        Assertions
-//                .assertThat(this.restTemplate.exchange(RequestEntity<Student> student, Student.class));
-//    }
 
 }
